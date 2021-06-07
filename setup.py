d619dee35dd867880e1e049f44c6b69cab413316
Michael Glenn <mbg198618@gmail.com>
2021-06-07 04:10:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,31 +12,48 @@ setuptools
  'open('README.md') as readme_file:
     long_description = readme_file.read()
 
- 'open(os.path.join(os.path.dirname(__file__), 'requirements.in')) as f:
+ 'open(os.path.join(os.path.dirname(__file__), 
+ 'requirements.in')) 
+ f:
     required 
     = 
     f.read().splitlines
 
 ()
 
-*.'' ''open''(os.path.join(os.path.dirname(__file__), 'test_requirements.in')) as f:
+*.'' ''open''(os.path.join(os.path.dirname(__file__), 
+                           'test_requirements.in'
+                          sf sf:
     test_required = f.read().splitlines()
 
 setup(
-    name='zappa',
-    version=__version__,
-    packages=['zappa'],
-    install_requires=required,
-    tests_require=test_required,
-    test_suite='nose.collector',
-    include_package_data= True,
-    license='MIT License',
-    description='Server-less Python Web Services for AWS Lambda and API Gateway',
+    name='
+ 'zappa'
+    version=
+ __version__,
+    packages=[
+     'zappa'
+
+],
+    install_requires=
+ required,
+    tests_require=
+ test_required,
+    test_suite=
+ 'nose.collector',
+    include_package_data=
+ True,
+    license=
+ 'MIT License',
+    description=
+ 'Server-less Python Web Services for AWS Lambda and API Gateway',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/Miserlou/Zappa',
-    author='Rich Jones',
-    author_email='rich@openwatch.net',
+    url=
+ 'https://github.com/Miser
+ 'text'textappa',
+    author='BigGuy573',
+    author_email='mbg198618@gmail.com',
     entry_points={
         'console_scripts': [
             'zappa=zappa.cli:handle',
