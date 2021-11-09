404f3cbe215377574d8a00d6acf37e1c2c762f2e
Tamás Hetesi <tams@tams.hu>
2021-11-09 16:18:52 +0100

    Don't install dependencies
    Allows the project using Zappa to declare Zappa dependencies themselves
    to avoid getting conflicts with their own deps.

--- setup.py
+++ setup.py
@@ -7,18 +7,10 @@ from zappa import __version__
 with open('README.md') as readme_file:
     long_description = readme_file.read()
 
-with open(os.path.join(os.path.dirname(__file__), 'requirements.in')) as f:
-    required = f.read().splitlines()
-
-with open(os.path.join(os.path.dirname(__file__), 'test_requirements.in')) as f:
-    test_required = f.read().splitlines()
-
 setup(
     name='zappa',
     version=__version__,
     packages=['zappa'],
-    install_requires=required,
-    tests_require=test_required,
     test_suite='nose.collector',
     include_package_data=True,
     license='MIT License',
