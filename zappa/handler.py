5bab60c4102e4bcfed6aa965e8936a89f7455f4a
Joon Hwan 김준환 <xncbf12@gmail.com>
2021-12-07 10:38:15 +0900

    UPDATE: apply to INSTANTIATE_LAMBDA_HANDLER_ON_IMPORT variable

--- zappa/handler.py
+++ zappa/handler.py
@@ -248,7 +248,8 @@ class LambdaHandler:
 
     @classmethod
     def lambda_handler(cls, event, context):  # pragma: no cover
-        handler = cls()
+        if not os.environ.get("INSTANTIATE_LAMBDA_HANDLER_ON_IMPORT"):
+            handler = cls()
         exception_handler = handler.settings.EXCEPTION_HANDLER
         try:
             return handler.handler(event, context)
@@ -665,4 +666,5 @@ def keep_warm_callback(event, context):
     # be triggered.
 
 
-LambdaHandler()
+if os.environ.get("INSTANTIATE_LAMBDA_HANDLER_ON_IMPORT"):
+    handler = LambdaHandler()
