dd74aac8d616a77ca7caf966dac4e9c8d6fed7f8
thejanitor213 <brucebeck995@gmail.com>
2022-03-18 12:25:21 +0200

    Be able to pass in a batch window when using batch size

--- zappa/utilities.py
+++ zappa/utilities.py
@@ -257,6 +257,10 @@ def get_event_source(
             super().__init__(context, config)
             self._lambda = kappa.awsclient.create_client("lambda", context.session)
 
+        @property
+        def batch_window(self):
+            return self._config.get("batch_window", 1 if self.batch_size > 10 else 0)
+
         def _get_uuid(self, function):
             uuid = None
             response = self._lambda.call(
@@ -276,6 +280,7 @@ def get_event_source(
                     FunctionName=function.name,
                     EventSourceArn=self.arn,
                     BatchSize=self.batch_size,
+                    MaximumBatchingWindowInSeconds=self.batch_window,
                     Enabled=self.enabled,
                 )
                 LOG.debug(response)
@@ -314,6 +319,7 @@ def get_event_source(
                     response = self._lambda.call(
                         "update_event_source_mapping",
                         BatchSize=self.batch_size,
+                        MaximumBatchingWindowInSeconds=self.batch_window,
                         Enabled=self.enabled,
                         FunctionName=function.arn,
                     )
