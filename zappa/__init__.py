9a1c332cecffeb6cb7d85a7a122666a9d6ebc332
monkut <shane.cousins@gmail.com>
2022-07-15 15:25:02 +0900

    ⏪️revert __version__ back to available pypi version
    Without a pypi available release the `test_slim_handler` testcase fails.
    
    ```
    ERROR: No matching distribution found for zappa==0.55.1
    E...................
    ======================================================================
    ERROR: test_slim_handler (tests.tests.TestZappa)
    ----------------------------------------------------------------------
    Traceback (most recent call last):
      File "/home/runner/work/Zappa/Zappa/tests/tests.py", line 2211, in test_slim_handler
        zappa_cli.create_package()
      File "/home/runner/work/Zappa/Zappa/zappa/cli.py", line 2430, in create_package
        venv=self.zappa.create_handler_venv(),
      File "/home/runner/work/Zappa/Zappa/zappa/core.py", line 494, in create_handler_venv
        raise EnvironmentError("Pypi lookup failed")
    OSError: Pypi lookup failed
    -------------------- >> begin captured stdout << ---------------------
    ```

--- zappa/__init__.py
+++ zappa/__init__.py
@@ -9,4 +9,4 @@ if sys.version_info[:2] not in SUPPORTED_VERSIONS:
     ) + "Zappa (and AWS Lambda) support the following versions of Python: {}".format(formatted_supported_versions)
     raise RuntimeError(err_msg)
 
-__version__ = "0.55.1"
+__version__ = "0.54.2"
