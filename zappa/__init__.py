7100e2c6cce3859bbc15c6c5d00b96f2e2a10779
monkut <shane.cousins@gmail.com>
2022-07-15 13:49:15 +0900

    :pencil: change version 0.54.1 -> 0.55.1 :fire: remove 3.6 from SUPPORTED_VERSIONS

--- zappa/__init__.py
+++ zappa/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-SUPPORTED_VERSIONS = [(3, 6), (3, 7), (3, 8), (3, 9)]
+SUPPORTED_VERSIONS = [(3, 7), (3, 8), (3, 9)]
 
 if sys.version_info[:2] not in SUPPORTED_VERSIONS:
     formatted_supported_versions = ["{}.{}".format(*version) for version in SUPPORTED_VERSIONS]
@@ -9,4 +9,4 @@ if sys.version_info[:2] not in SUPPORTED_VERSIONS:
     ) + "Zappa (and AWS Lambda) support the following versions of Python: {}".format(formatted_supported_versions)
     raise RuntimeError(err_msg)
 
-__version__ = "0.54.1"
+__version__ = "0.55.1"
