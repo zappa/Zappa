d7fcee4cd1994a522e50bc157d21325ae28df645
monkut <shane.cousins@gmail.com>
2022-07-28 13:48:33 +0900

    :recycle: refactor to allow for other binary ignore types based on mimetype. (currently openapi schema can't be passed as text.

--- zappa/handler.py
+++ zappa/handler.py
@@ -560,22 +560,19 @@ class LambdaHandler:
                         # We base64 encode for two reasons when BINARY_SUPPORT is enabled:
                         # - Content-Encoding is present, which is commonly used by compression mechanisms to indicate
                         #   that the content is in br/gzip/deflate/etc encoding
-                        #   (Related: https://github.com/zappa/Zappa/issues/908). Content like this must be
-                        #   transmitted as b64.
-                        # - The response is assumed to be some binary format (since BINARY_SUPPORT is enabled and it
-                        #   isn't application/json or text/)
-                        if settings.BINARY_SUPPORT and response.headers.get("Content-Encoding"):
-                            zappa_returndict["body"] = base64.b64encode(response.data).decode()
-                            zappa_returndict["isBase64Encoded"] = True
-                        elif (
-                            settings.BINARY_SUPPORT
-                            and not response.mimetype.startswith("text/")
-                            and response.mimetype != "application/json"
-                        ):
-                            zappa_returndict["body"] = base64.b64encode(response.data).decode("utf8")
-                            zappa_returndict["isBase64Encoded"] = True
-                        else:
-                            zappa_returndict["body"] = response.get_data(as_text=True)
+                        #   (Related: https://github.com/zappa/Zappa/issues/908).
+                        #   Content like this must be transmitted as b64.
+                        # - The response is assumed to be some binary format (since BINARY_SUPPORT is enabled and it isn't application/json or text/)
+                        zappa_returndict["body"] = response.get_data(as_text=True)
+                        if settings.BINARY_SUPPORT:
+                            # overwrite zappa_returndict["body"] if necessary
+                            exclude_startswith_mimetypes = ("text/", "application/json", "application/vnd.oai.openapi")  # TODO: consider for settings
+                            if response.headers.get("Content-Encoding"):  # Assume br/gzip/deflate/etc encoding
+                                zappa_returndict["body"] = base64.b64encode(response.data).decode("utf8")
+                                zappa_returndict["isBase64Encoded"] = True
+                            elif not response.mimetype.startswith(exclude_startswith_mimetypes):
+                                zappa_returndict["body"] = base64.b64encode(response.data).decode("utf8")
+                                zappa_returndict["isBase64Encoded"] = True
 
                     zappa_returndict["statusCode"] = response.status_code
                     if "headers" in event:
