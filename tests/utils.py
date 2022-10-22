58e221b629ac7a155ca652c981f1f018b8d2c3ce
monkut <shane.cousins@gmail.com>
2022-10-22 11:06:46 +0900

    :art: run black/isort

--- tests/utils.py
+++ tests/utils.py
@@ -84,4 +84,3 @@ def get_unsupported_sys_versioninfo() -> tuple:
     """Mock used to test the python unsupported version testcase"""
     invalid_versioninfo = namedtuple("version_info", ["major", "minor", "micro", "releaselevel", "serial"])
     return invalid_versioninfo(3, 6, 1, "final", 0)
-
