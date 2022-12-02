1e3466ecf63994e0218fef9f562bd712f041aa67
Gabor Schulz <gabor@gaborschulz.com>
2022-12-02 05:23:31 +0100

    Remove version constraints

--- zappa/__init__.py
+++ zappa/__init__.py
@@ -7,6 +7,6 @@ if sys.version_info[:2] not in SUPPORTED_VERSIONS:
     err_msg = "This version of Python ({}.{}) is not supported!\n".format(
         *sys.version_info
     ) + "Zappa (and AWS Lambda) support the following versions of Python: {}".format(formatted_supported_versions)
-    raise RuntimeError(err_msg)
+    # raise RuntimeError(err_msg)
 
 __version__ = "0.55.0"
