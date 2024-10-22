629da5ebcced14c3769ea623319dacf21ef8b131
shane <shane.cousins@kiconiaworks.com>
2024-10-22 15:39:00 +0900

    :art: :wrench: fix flake8/black > change type() check method from using type to using isinstance()

--- zappa/middleware.py
+++ zappa/middleware.py
@@ -47,12 +47,12 @@ class ZappaWSGIMiddleware:
             new_headers = [
                 header
                 for header in headers
-                if ((type(header[0]) != str) or (header[0].lower() != "set-cookie"))
+                if isinstance(header[0], str) or header[0].lower() != "set-cookie"
             ]
             cookie_headers = [
                 (header[0].lower(), header[1])
                 for header in headers
-                if ((type(header[0]) == str) and (header[0].lower() == "set-cookie"))
+                if isinstance(header[0], str) and header[0].lower() == "set-cookie"
             ]
             new_headers = new_headers + cookie_headers
 
