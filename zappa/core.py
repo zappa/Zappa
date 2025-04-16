4b73260fdaf71cb97853eb2e78b5e9ba188e51fe
monkut <shane.cousins@gmail.com>
2025-04-16 13:15:16 +0900

    :bug: :wrench: fix Zappa.create_handler_venv win32 case wherestderror_result is None (#1353)
    * :bug: :wrench: (#1342) fix Zappa.create_handler_venv win32 case where stderror_result is None
    
    * :wrench: :art: fix flake8 errors
    
    * :art: :wrench: fix flake8/black > change type() check method from using type to using isinstance()
    
    * :wrench: run black, `black --line-length 127 .`
    
    * :wrench: :white_check_mark: fix `test_create_handler_venv_win32_none_stderror_result`　PopenMock
    
    ---------
    
    Co-authored-by: shane <shane.cousins@kiconiaworks.com>

--- zappa/core.py
+++ zappa/core.py
@@ -310,7 +310,7 @@ class Zappa:
         self.manylinux_wheel_file_match = re.compile(
             rf'^.*{self.manylinux_suffix_start}-(manylinux_\d+_\d+_x86_64[.])?manylinux({"|".join(self.manylinux_suffixes)})_x86_64[.]whl$'  # noqa: E501
         )
-        self.manylinux_wheel_abi3_file_match = re.compile(rf"^.*cp3.-abi3-manylinux.*_x86_64[.]whl$")
+        self.manylinux_wheel_abi3_file_match = re.compile(r"^.*cp3.-abi3-manylinux.*_x86_64[.]whl$")
 
         self.endpoint_urls = endpoint_urls
         self.xray_tracing = xray_tracing
@@ -488,9 +488,9 @@ class Zappa:
 
         if pip_return_code:
             logger.info("command: %s", " ".join(command))
-            if stdout_result.strip():
+            if stdout_result and stdout_result.strip():
                 logger.info("stdout: %s", stdout_result.strip())
-            if stderror_result.strip():
+            if stderror_result and stderror_result.strip():
                 logger.error("stderr: %s", stderror_result)
             raise EnvironmentError("Pypi lookup failed")
 
