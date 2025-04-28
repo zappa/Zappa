40a738f87dd1bf747cd43a0d83ca70e2f6ebef56
shane <shane.cousins@kiconiaworks.com>
2025-04-28 10:56:57 +0900

    :fire: remove 3.8 python version classifier from setup()

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
