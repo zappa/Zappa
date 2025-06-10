1fa15f26fb6a2fac68e028b483755d4b0bb0f14f
shane <shane.cousins@kiconiaworks.com>
2025-06-10 12:15:05 +0900

    :wrench: replace os.path related items with pathlib Path usage :wrench: change print() to LOG statement

--- zappa/utilities.py
+++ zappa/utilities.py
@@ -1,7 +1,6 @@
 import calendar
 import datetime
 import fnmatch
-import io
 import json
 import logging
 import os
@@ -149,16 +148,16 @@ def detect_django_settings():
     """
 
     matches = []
-    for root, dirnames, filenames in os.walk(Path.cwd()):
+    cwd = Path.cwd()
+    for root, dirnames, filenames in os.walk(cwd):
+        root_directory = Path(root).resolve()
         for filename in fnmatch.filter(filenames, "*settings.py"):
-            full = os.path.join(root, filename)
-            if "site-packages" in full:
+            full_filepath = root_directory / filename
+            if "site-packages" in str(full_filepath):
                 continue
-            full = os.path.join(root, filename)
-            package_path = full.replace(os.getcwd(), "")
-            package_module = package_path.replace(os.sep, ".").split(".", 1)[1].replace(".py", "")
-            print("Detected Django settings file: {}".format(package_module))
-
+            package_path = full_filepath.relative_to(cwd)
+            package_module = ".".join(package_path.parts).replace(".py", "")
+            LOG.info(f"Detected Django settings file: {package_module}")
             matches.append(package_module)
     return matches
 
@@ -170,15 +169,15 @@ def detect_flask_apps() -> list[str]:
     """
 
     matches = []
-    for root, dirnames, filenames in os.walk(os.getcwd()):
+    cwd = Path.cwd()
+    for root, dirnames, filenames in os.walk(cwd):
+        root_directory = Path(root).resolve()
         for filename in fnmatch.filter(filenames, "*.py"):
-            full = os.path.join(root, filename)
-            if "site-packages" in full:
+            full_filepath = root_directory / filename
+            if "site-packages" in str(full_filepath):
                 continue
 
-            full = os.path.join(root, filename)
-
-            with io.open(full, "r", encoding="utf-8") as f:
+            with full_filepath.open("r", encoding="utf-8") as f:
                 lines = f.readlines()
                 for line in lines:
                     app = None
@@ -192,10 +191,9 @@ def detect_flask_apps() -> list[str]:
                     if not app:
                         continue
 
-                    package_path = full.replace(os.getcwd(), "")
-                    package_module = package_path.replace(os.sep, ".").split(".", 1)[1].replace(".py", "")
-                    app_module = package_module + "." + app
-
+                    package_path = full_filepath.relative_to(cwd)
+                    package_module = ".".join(package_path.parts).replace(".py", "")
+                    app_module = f"{package_module}.{app}"
                     matches.append(app_module)
 
     return matches
