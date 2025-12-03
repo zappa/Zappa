96532e1ac564a4ac86f5e0c3c7c6de6995c89f90
magicmirror-peter <peter@magicmirrorsecurity.com>
2025-12-03 09:25:35 -0800

    Adds support for Python 3.14
    Copies the same structure used by the PR that added support for 3.13
    
    Found here: https://github.com/zappa/Zappa/pull/1364

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Framework :: Django",
         "Framework :: Django :: 4.2",
         "Framework :: Django :: 5.2",
